8b377a8bd177e5aa7d25eb977c51977ea8f9c029
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-04-15 19:35:49 +0900

    animation on player movement

--- packages/emojimon-unity/Assets/Scripts/PlayerController.cs
+++ packages/emojimon-unity/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ public class PlayerController : MonoBehaviour
 {
     private Vector3 targetPos;
     public float damping = 1;
+    private Animator animator;
 
     protected void Start()
     {
+        animator = GetComponentsInChildren<Animator>()[0];
+
         GameStateMediator.Instance.EventStateUpdated += OnStateUpdate;
     }
 
@@ -32,5 +35,15 @@ public class PlayerController : MonoBehaviour
             targetPos,
             damping * Time.deltaTime
         );
+
+        var remainingDist = Vector3.Distance(this.transform.localPosition, targetPos);
+        if (remainingDist > 0.1)
+        {
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
     }
 }
