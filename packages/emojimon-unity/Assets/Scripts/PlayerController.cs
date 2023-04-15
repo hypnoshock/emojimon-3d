deb609448a9b4ab732a7a8b1cbcec54d5fab1845
Jimmy Whitwell <jimmy.whitwell@playmint.com>
2023-04-15 12:08:57 +0900

    removed that offset. It was weird

--- packages/emojimon-unity/Assets/Scripts/PlayerController.cs
+++ packages/emojimon-unity/Assets/Scripts/PlayerController.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    // We'll probably get rid of this by putting the caracter in a parent object and setting the paren'ts
-    // position to the desired offset but I'm not sure what structure we're going with yet so I'll use an offset
-    public Vector3Int Offset;
-
     protected void Start()
     {
         GameStateMediator.Instance.EventStateUpdated += OnStateUpdate;
@@ -16,7 +12,7 @@ public class PlayerController : MonoBehaviour
     {
         var pos = gs.PlayerPosition;
         this.transform.SetLocalPositionAndRotation(
-            new Vector3(pos.X + Offset.x, this.transform.position.y, -pos.Y + Offset.z),
+            new Vector3(pos.X, this.transform.position.y, -pos.Y),
             Quaternion.identity
         );
     }
